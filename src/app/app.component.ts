40346badb93d402c593c4cd91302e1b74931c0e8
Alexander <duvanhoyos@hotmail.com>
2019-01-23 18:15:35 -0500

    Creando pipes v1 50 %

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { promise } from "protractor";
+import { reject } from "q";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Angular-Pipes';
+  title = "Angular-Pipes";
+
+  nombre = "Alexander";
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: { calle: "x", casa: "12" }
+  };
+
+  valorDePromesa = new Promise( ( resove, reject) => {
+    setTimeout( () => resove('Llego la data!'), 3500 );
+  })
+
+
+  fecha = '2019-01-23';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LOCALE_ID } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -12,7 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es-CO' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
